75124e1888ed611441a22e1be6eceaf9512f33e3
Nik Vangerow <n@vangerow.uk>
2018-05-29 22:11:00 +0100

    improve login screen, add moving between fields

--- ios/LogInScreenController.swift
+++ ios/LogInScreenController.swift
@@ -3,7 +3,9 @@ import UIKit
 class LogInScreenController: UIViewController {
     
     private var shifter = KeyboardShifter()
-
+    @IBOutlet var topConstraint: NSLayoutConstraint!
+    @IBOutlet weak var logo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,11 +19,29 @@ extension LogInScreenController: KeyboardShifterDelegate {
     
     func keyboard(_ keyboardShifter: KeyboardShifter, shiftedBy delta: CGFloat, duration: Double, options: UIViewAnimationOptions) {
         view.frame.origin.y += delta
-        
+        topConstraint?.constant -= delta
+
         UIView.animate(withDuration: duration,
                        delay: 0,
-                       options: options, animations: {}) { [weak view = view] _ in
-                        view?.layoutIfNeeded()
+                       options: options,
+                       animations: {},
+                       completion: { [weak view = view] _ in
+                           self.logo.layoutIfNeeded()
+                           view?.layoutIfNeeded()
+                       })
+    }
+}
+
+extension LogInScreenController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        let nextTag = textField.tag + 1
+        
+        if let nextResponder = textField.superview?.viewWithTag(nextTag) {
+            nextResponder.becomeFirstResponder()
+        } else {
+            textField.resignFirstResponder()
         }
+        
+        return true
     }
 }
