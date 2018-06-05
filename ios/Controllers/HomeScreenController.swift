a3d8653256059ec6e3b839a87d3b48107d471986
Nik Vangerow <n@vangerow.uk>
2018-06-05 20:42:42 +0100

    remove spaces, change to color literals, extract common code, fix layouts

--- ios/Controllers/HomeScreenController.swift
+++ ios/Controllers/HomeScreenController.swift
@@ -4,10 +4,10 @@ class HomeScreenController: UIViewController {
 
     // swiftlint:disable line_length
     static let strings = [
-        "directAction": " Directly intervene, for example, by asking the person to stop. Immediately act or call out negative behaviour, explaining why it is not OK.",
-        "delay": " Wait for the situation to pass and check, with individual. Take action at a later stage when you have had time to consider. It’s never too late to act.",
-        "distraction": " Indirectly intervene, for example, de-escalating by interrupting or changing the subject or focus. Useful where the direct approach may be harmful to the target or bystander.",
-        "delegation": " Inform a more senior member of staff, for example, your Head of Department, Director or Manager. Use someone with the social power or authority to deal with it."
+        "directAction": NSLocalizedString("Directly intervene, for example, by asking the person to stop. Immediately act or call out negative behaviour, explaining why it is not OK.", comment: ""),
+        "delay": NSLocalizedString("Wait for the situation to pass and check, with individual. Take action at a later stage when you have had time to consider. It’s never too late to act.", comment: ""),
+        "distraction": NSLocalizedString("Indirectly intervene, for example, de-escalating by interrupting or changing the subject or focus. Useful where the direct approach may be harmful to the target or bystander.", comment: ""),
+        "delegation": NSLocalizedString("Inform a more senior member of staff, for example, your Head of Department, Director or Manager. Use someone with the social power or authority to deal with it.", comment: "")
     ]
     // swiftlint:enable line_length
 
@@ -26,23 +26,21 @@ class HomeScreenController: UIViewController {
             return
         }
 
+        fourDsController.textBodyDescription = HomeScreenController.strings[identifier]!
+
         switch identifier {
         case "directAction":
             segue.destination.navigationItem.title = "Direct Action"
-            fourDsController.textBodyDescription = HomeScreenController.strings[identifier]!
-            fourDsController.view.backgroundColor = UIColor(red: 0, green: 61/255, blue: 113/255, alpha: 1)
+            fourDsController.view.backgroundColor = #colorLiteral(red: 0, green: 0.2392156863, blue: 0.4431372549, alpha: 1)
         case "delay":
             segue.destination.navigationItem.title = "Delay"
-            fourDsController.textBodyDescription = HomeScreenController.strings[identifier]!
-            fourDsController.view.backgroundColor = UIColor(red: 0, green: 187/255, blue: 200/255, alpha: 1)
+            fourDsController.view.backgroundColor = #colorLiteral(red: 0, green: 0.7333333333, blue: 0.7843137255, alpha: 1)
         case "distraction":
             segue.destination.navigationItem.title = "Distraction"
-            fourDsController.textBodyDescription = HomeScreenController.strings[identifier]!
-            fourDsController.view.backgroundColor = UIColor(red: 0, green: 15/255, blue: 52/255, alpha: 1)
+            fourDsController.view.backgroundColor = #colorLiteral(red: 0, green: 0.05882352941, blue: 0.2039215686, alpha: 1)
         case "delegation":
             segue.destination.navigationItem.title = "Delegation"
-            fourDsController.textBodyDescription = HomeScreenController.strings[identifier]!
-            fourDsController.view.backgroundColor = UIColor(red: 243/255, green: 45/255, blue: 75/255, alpha: 1)
+            fourDsController.view.backgroundColor = #colorLiteral(red: 0.9529411765, green: 0.1764705882, blue: 0.2941176471, alpha: 1)
         default:
             return
         }
