81bc159c43395b57ee7cc6459497c6f3edf6561a
Nik Vangerow <n@vangerow.uk>
2018-06-06 20:57:25 +0100

    add tests for Authentication and fix some issues with credential retention

---
+++ ios/TestDelegate.swift
@@ -0,0 +1,4 @@
+import UIKit
+
+class TestDelegate: UIResponder, UIApplicationDelegate {
+}
---
+++ ios/main.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+import UIKit
+
+let isRunningTests = NSClassFromString("XCTestCase") != nil
+let appDelegateClass = isRunningTests ? NSStringFromClass(TestDelegate.self) : NSStringFromClass(AppDelegate.self)
+let args = UnsafeMutableRawPointer(CommandLine.unsafeArgv)
+    .bindMemory(to: UnsafeMutablePointer<Int8>.self, capacity: Int(CommandLine.argc))
+UIApplicationMain(CommandLine.argc, args, nil, appDelegateClass)
