4709d199d2f69f4445ccbdbd6224270825581665
Enyi Shang <enyi.shang16@imperial.ac.uk>
2018-06-12 15:06:08 +0100

    help area table view

--- ios/Controllers/ProfileScreenController.swift
+++ ios/Controllers/ProfileScreenController.swift
@@ -1,9 +1,30 @@
 import UIKit
 
 class ProfileScreenController: UIViewController {
+    let helpArea = ["Sexual Harassment", "Verbal Assault", "Racism"]
+
+    @IBOutlet weak var helpAreaTable: UITableView!
 
     @IBAction func logOutButtonPress(_ sender: UIButton) {
         Environment.userAuth.logOut()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        helpAreaTable.reloadData()
+    }
+
+}
+
+extension ProfileScreenController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return helpArea.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "helpAreaCell", for: indexPath)
+        cell.textLabel?.text = helpArea[indexPath.row]
+        return cell
     }
 
 }
