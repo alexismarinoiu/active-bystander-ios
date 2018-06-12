e352d9dc50144be67487c952f5368c7d45762ea1
Enyi Shang <enyi.shang16@imperial.ac.uk>
2018-06-12 17:29:10 +0100

    move the location tracking bar from setting to profile and add the help area table in editing screen

--- ios/Controllers/ProfileScreenController.swift
+++ ios/Controllers/ProfileScreenController.swift
@@ -1,7 +1,10 @@
 import UIKit
+import CoreLocation
 
 class ProfileScreenController: UIViewController {
-    let helpArea = ["Sexual Harassment", "Verbal Assault", "Racism"]
+    private let helpArea = ["Sexual Harassment", "Verbal Assault", "Racism"]
+    private weak var locationTrackingCell: SettingsSwitchCell?
+    private weak var locationManager: CLLocationManager?
 
     @IBOutlet weak var helpAreaTable: UITableView!
 
@@ -12,19 +15,70 @@ class ProfileScreenController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         helpAreaTable.reloadData()
+        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+            locationManager = appDelegate.locationManager
+            let selector = #selector(locationManagerChangedAuthorization(notification:))
+            appDelegate.notificationCenter.addObserver(self, selector: selector,
+                                                       name: .AVLocationAuthorizationNotification, object: nil)
+        }
     }
-
 }
 
 extension ProfileScreenController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return helpArea.count
+        return section == 0 ? helpArea.count : 1
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "helpAreaCell", for: indexPath)
-        cell.textLabel?.text = helpArea[indexPath.row]
-        return cell
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "helpAreaCell", for: indexPath)
+            cell.textLabel?.text = helpArea[indexPath.row]
+            return cell
+        }
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "switch", for: indexPath)
+        guard let switchCell = cell as? SettingsSwitchCell else {
+            return cell
+        }
+
+        locationTrackingCell = switchCell
+        switchCell.kind = .locationTracking
+        switchCell.titleLabel.text = switchCell.kind?.label
+        switchCell.action = { [weak locationManager] (isOn: Bool) -> Void in
+            if isOn {
+                locationManager?.requestAlwaysAuthorization()
+            }
+            UserDefaults.this.isLocationEnabled = isOn
+        }
+        switchCell.toggleSwitch.isOn = UserDefaults.this.isLocationEnabled
+        return switchCell
     }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Help Area"
+        default:
+            return "General"
+        }
+    }
+}
+
+extension ProfileScreenController {
+    @objc func locationManagerChangedAuthorization(notification: Notification) {
+        guard let status = notification.userInfo?[0] as? CLAuthorizationStatus else {
+            return
+        }
+
+        if !CLLocationManager.locationServicesEnabled() || status != .authorizedAlways {
+            UserDefaults.this.isLocationEnabled = false
+            if locationTrackingCell?.kind == .locationTracking {
+                locationTrackingCell?.toggleSwitch.isOn = false
+            }
+        }
+    }
 }
