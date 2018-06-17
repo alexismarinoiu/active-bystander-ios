1d3e994cdd8fbfe4aa49fc2f38b61c04639ff8a4
Enyi Shang <enyi.shang16@imperial.ac.uk>
2018-06-17 13:21:03 +0100

    scroll to the form directly

--- ios/Controllers/FeedbackScreenController.swift
+++ ios/Controllers/FeedbackScreenController.swift
@@ -3,12 +3,25 @@ import WebKit
 
 class FeedbackScreenController: UIViewController {
     @IBOutlet weak var webView: WKWebView!
+    let abURL = "https://www.imperial.ac.uk/engineering/staff/human-resources/active-bystander/"
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let myURL = URL(string: "https://www.imperial.ac.uk/engineering/staff/human-resources/active-bystander/")
-        let myRequest = URLRequest(url: myURL!)
+        let webViewURL = URL(string: abURL)
+        let myRequest = URLRequest(url: webViewURL!)
+        webView.navigationDelegate = self
         webView.load(myRequest)
+    }
+}
+
+extension FeedbackScreenController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        if webView.url?.absoluteString == abURL {
+            // swiftlint:disable line_length
+            let javaScript = "var form = document.getElementsByClassName(\"qualtrics-widget responsive-iframe\")[0]; form.scrollIntoView();"
+            // swiftlint:enable line_length
+            webView.evaluateJavaScript(javaScript, completionHandler: nil)
+        }
     }
 }
